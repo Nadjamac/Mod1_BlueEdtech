1d68808382c39d238de94d1925686e8726b0b54c
Ycaro <ycarobatalha1@gmail.com>
2021-05-11 21:27:43 -0300

    Exemplo de commit

---
+++ Aula 07_CodeLAB.py
@@ -0,0 +1,16 @@
+#Lista de Exercicios_Aula06
+
+#1
+def soma(x, y, z):
+    return x + y + z
+
+numero1 = float(input("Digite um número: "))
+
+numero2 = float(input("Digite um número: "))
+
+numero3 = float(input("Digite um número: "))
+
+resultado = soma(numero1, numero2, numero3)
+
+print(f"O resultado da soma é: {resultado}")
+
