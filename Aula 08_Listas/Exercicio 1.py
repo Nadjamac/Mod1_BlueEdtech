1731c91771bee327cba6c89cb070faf20a74e743
Ycaro <ycarobatalha1@gmail.com>
2021-05-14 17:21:50 -0300

    resolucao do exercicio 1

---
+++ Aula 08_Listas/Exercicio 1.py
@@ -0,0 +1,11 @@
+l = [5 , 7 , 2 , 9 , 4 , 1 , 3]
+
+print("O tamanho da lista é:", len(l))
+print("O maior valor da lista é:", max(l))
+print("O menor valor da lista é:", min(l))
+print("A soma dos valores da lista é:", sum(l))
+
+l.sort()
+print(l)
+l.reverse()
+print(l)
