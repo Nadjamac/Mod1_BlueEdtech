624d12dfa7fa3751afc217fea7ca0f955256ccf6
Ycaro <ycarobatalha1@gmail.com>
2021-05-21 14:11:10 -0300

    Exercicios Aula 06 e Aula 07

--- ListaExercicios_Aula07/Exercicio 3.py
+++
@@ -1,6 +0,0 @@
-def maiuscula(string):
-    return string.upper()
-
-resposta = maiuscula("eu sou o ycaro!")
-
-print(resposta)
--- ListaExercicios_Aula07/Exercicio 5.py
+++
@@ -1,8 +0,0 @@
-def ficha(x = "Gabigol" , y = "10"):
-    return print("FICHA - ", x, ":", y , "gols.") 
-
-nome = input("Digite o nome do jogador: ")
-gols = input("Digite a quantidade de gols marcados: ")
-
-ficha()
-
